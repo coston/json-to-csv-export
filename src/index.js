b204049ad8fc59d01b4d627f69eada38d0b76576
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2022-01-24 21:29:43 +0700

    feat: add delimiter option (#17)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-const csvDownload = (data, name) => {
+const csvDownload = (data, name, delimiter) => {
   const items = data
   const filename = name || `export.csv`
+  const d = delimiter || `,`
 
   const header = Array.from(
     new Set(items.reduce((r, e) => [...r, ...Object.keys(e)], []))
   )
   let csv = items.map(row =>
-    header.map(fieldName => JSON.stringify(row[fieldName] || '')).join(',')
+    header.map(fieldName => JSON.stringify(row[fieldName] || '')).join(d)
   )
-  csv.unshift(header.join(','))
+  csv.unshift(header.join(d))
   csv = csv.join('\r\n')
 
   const blob = new Blob([csv], {
