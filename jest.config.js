9bace222471a65fe581874a3d6c8f32f9fffb8b7
Sergey Korsik <serg.korsik@gmail.com>
2022-08-19 07:17:07 +0200

    #27 Changed test env back to node

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json'
