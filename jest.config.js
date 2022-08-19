7af7b3691c9448f5fcc71ad1181f7af4c775e6d9
Sergey Korsik <serg.korsik@gmail.com>
2022-08-19 06:59:06 +0200

    #27 Removed idea files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json'
