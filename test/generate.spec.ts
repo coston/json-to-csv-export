d1221b871af8c7b4f0a200cd62453e552563d5fc
Zach McElrath <zmcelrath87@gmail.com>
2024-07-30 20:00:49 -0400

    feat: Change default delimiter to semicolon (#64)
    closes #61

--- test/generate.spec.ts
+++ test/generate.spec.ts
@@ -1,23 +1,72 @@
 import { csvGenerateRow } from "../src/generate";
 
-test("csv generator correctly handles custom delimiters", () => {
-  const mockData = {
-    id: 1,
-    text: "Lee Perry",
-  };
+describe("csvGenerateRow", () => {
 
-  expect(csvGenerateRow(mockData, ["id", "text"], ",")).toEqual(
-    `1,"Lee Perry"`
-  );
-});
+  test("correctly handles empty, number, and boolean values", () => {
+    const mockData = {
+      id: 0,
+      one: true,
+      two: false,
+      empty: "",
+    };
+  
+    expect(csvGenerateRow(mockData, ["id", "one", "two", "empty"], ",")).toEqual(
+      `0,true,false,`
+    );
+  });
 
-test("row generator correctly handles data with double-quotes", () => {
-  const mockData = {
-    id: 1,
-    text: 'Lee "Scratch" Perry',
-  };
+  test("correctly handles custom delimiters", () => {
+    const mockData = {
+      id: 1,
+      text: "Lee Perry",
+    };
+  
+    expect(csvGenerateRow(mockData, ["id", "text"], ";")).toEqual(
+      `1;Lee Perry`
+    );
+  });
+  
+  test("correctly handles data with double-quotes", () => {
+    const mockData = {
+      id: 1,
+      text: 'Lee "Scratch" Perry',
+    };
+    expect(csvGenerateRow(mockData, ["id", "text"], ";")).toEqual(
+      `1;"Lee ""Scratch"" Perry"`
+    );
+  });
 
-  expect(csvGenerateRow(mockData, ["id", "text"], ";")).toEqual(
-    `1;"Lee ""Scratch"" Perry"`
-  );
+  test("correctly handles data with carriage return / newline and whitespace", () => {
+    const mockData = {
+      id: 1,
+      text: `Lee
+Perry  `,
+    };
+    expect(csvGenerateRow(mockData, ["id", "text"], ",")).toEqual(
+      `1,"Lee
+Perry  "`
+    );
+  });
+  
+  test("correctly handles data containing the delimiter (semicolon)", () => {
+    const mockData = {
+      id: 1,
+      text: 'Bond; James Bond',
+    };
+    expect(csvGenerateRow(mockData, ["id", "text"], ";")).toEqual(
+      `1;"Bond; James Bond"`
+    );
+  });
+
+  test("correctly handles data containing the delimiter (comma)", () => {
+    const mockData = {
+      id: 1,
+      name: 'Baggins, Frodo',
+      location: 'The Shire; Eriador',
+    };
+    expect(csvGenerateRow(mockData, ["id", "name", "location"], ",")).toEqual(
+      `1,"Baggins, Frodo",The Shire; Eriador`
+    );
+  });
+  
 });
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,10 +1,35 @@
 import csvDownload from "../src/index";
 import mockData from "./__mocks__/mockData";
 
+// current version of JSDom doesn't support Blob.text(), so this is a FileReader-based workaround.
+const getBlobAsText = async (blob: Blob, encoding = "text/csv;charset=utf-8;"): Promise<string> => {
+  const fileReader = new FileReader();
+  return new Promise((resolve) => {
+    fileReader.onload = () => {
+      resolve(fileReader.result as string);
+    };
+    fileReader.readAsText(blob, encoding);
+  });
+};
+
 describe("csvDownload", () => {
   const _URL = global.URL;
+  let capturedBlob: Blob | null;
+  let link;
 
-  global.URL.createObjectURL = () => "test/url";
+  beforeAll(() => {
+    global.URL.createObjectURL = (blob: Blob) => {
+      capturedBlob = blob;
+      return "test/url";
+    };
+  })
+
+  beforeEach(() => {
+    document.onclick = (e) => {
+      link = e.target as HTMLAnchorElement;
+    };
+    capturedBlob = null;
+  });
 
   afterEach(() => {
     global.URL = _URL;
@@ -18,29 +43,27 @@ describe("csvDownload", () => {
     csvDownload({ data: [] });
   });
 
-  test("with data", async () => {
-    let link;
-
-    document.onclick = (e) => {
-      link = e.target as HTMLAnchorElement;
-    };
+  test("with data, using comma delimiter as default", async () => {
     csvDownload({ data: mockData });
     expect(link.href).toContain("test/url");
     expect(link.download).toEqual("export.csv");
+    expect(capturedBlob).not.toBe(null);
+    const generatedCsvString = await getBlobAsText(capturedBlob as Blob);
+    expect(generatedCsvString.startsWith(`id,First Name,Last Name,Email,Gender,IP Address`)).toBeTruthy();
+    expect(generatedCsvString.includes(`1,Blanch,Elby,belby0@bing.com,Female,112.81.107.207`)).toBeTruthy();
   });
 
   test("with all properties provided", async () => {
-    let link;
-
-    document.onclick = (e) => {
-      link = e.target as HTMLAnchorElement;
-    };
     csvDownload({
       data: mockData,
       headers: ["ID", "Name", "Surname", "E-mail", "Gender", "IP"],
       filename: "custom-name",
-      delimiter: ",",
+      delimiter: ";",
     });
     expect(link.download).toEqual("custom-name.csv");
+    expect(capturedBlob).not.toBe(null);
+    const generatedCsvString = await getBlobAsText(capturedBlob as Blob);
+    expect(generatedCsvString.startsWith(`ID;Name;Surname;E-mail;Gender;IP`)).toBeTruthy();
+    expect(generatedCsvString.includes(`1;Blanch;Elby;belby0@bing.com;Female;112.81.107.207`)).toBeTruthy();
   });
 });
